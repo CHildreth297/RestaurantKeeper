d642e1ca912e8ae460dfacfff276f5b3255aa930
Daiwik Swaminathan <daiwik.swaminathan@gmail.com>
2025-03-01 13:05:22 -0800

    finished adding basic routing. app starts in home screen. mock entries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.coil.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.navigation.compose)
 
 
     testImplementation(libs.junit)
--- app/src/main/java/com/zybooks/restaurantkeeper/HomeViewModel.kt
+++ app/src/main/java/com/zybooks/restaurantkeeper/HomeViewModel.kt
@@ -2,10 +2,11 @@ package com.zybooks.restaurantkeeper
 
 import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
+import java.time.LocalDate
 
 // Sealed class for MediaItem
 sealed class MediaItem {
-    data class Entry(val id: Int, val title: String) : MediaItem()
+    data class Entry(val id: Int, val title: String, val location: String, val rating: Int, val comments: String) : MediaItem()
     data class Collection(val id: Int, val name: String, val items: List<Entry>) : MediaItem()
 }
 
